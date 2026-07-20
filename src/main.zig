c7063f1d65c38c23be1f532e242c8c661d417760
nam <namish@tuta.io>
2026-07-20 18:57:37 +0530

    current code is batshit borken im just saving

--- src/main.zig
+++ src/main.zig
@@ -4,21 +4,35 @@ const ParserState = struct {
     target_string: []const u8,
     result: ?[]const u8,
     index: usize,
-
-    fn init(target_string: []const u8) ParserState {
-        return .{ .target_string = target_string, .index = 0, .result = null };
-    }
-
-    fn str(self: ParserState, s: []const u8) ParserState {
-        if (std.mem.startsWith(u8, self.target_string, s)) {
-            return .{ .target_string = self.target_string, .result = s, .index = self.index + s.len };
-        }
-        return self;
-    }
 };
 
+fn new_parser(target_string: []const u8) ParserState {
+    return .{ .target_string = target_string, .index = 0, .result = null };
+}
+
+fn str(parser: ParserState, s: []const u8) !ParserState {
+    if (std.mem.startsWith(u8, parser.target_string, s)) {
+        return .{ .target_string = parser.target_string, .result = s, .index = parser.index + s.len };
+    }
+    return error.CouldNotMatch;
+}
+
+const ParseFunc = fn (ParserState, []const u8) ParserState;
+
+fn sequence(parser: ParserState, funcs: []ParseFunc) !ParserState {
+    const results = []ParserState;
+
+    var next_state = parser;
+    for (funcs) |f| {
+        next_state = f(parser);
+        results.add(f);
+    }
+
+    return .{ .index = parser.index, .result = results, .target_string = parser.target_string };
+}
+
 pub fn main() !void {
-    var p = ParserState.init("hello this is big text corpus");
-    const res = p.str("hello");
+    const p = new_parser("hello this is big text corpus");
+    const res = try str(p, "hello");
     std.debug.print("target_string = {s}\nresult = {s}\nindex = {d}", .{ res.target_string, res.result.?, res.index });
 }
