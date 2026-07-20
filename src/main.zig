57687f60d4148eb5407e051a588aca7c50ee1965
nam <namish@tuta.io>
2026-07-20 13:18:57 +0530

    save

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,24 @@
 const std = @import("std");
 
+const ParserState = struct {
+    target_string: []const u8,
+    result: ?[]const u8,
+    index: usize,
+
+    fn init(target_string: []const u8) ParserState {
+        return .{ .target_string = target_string, .index = 0, .result = null };
+    }
+
+    fn str(self: ParserState, s: []const u8) ParserState {
+        if (std.mem.startsWith(u8, self.target_string, s)) {
+            return .{ .target_string = self.target_string, .result = s, .index = self.index + s.len };
+        }
+        return self;
+    }
+};
+
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var p = ParserState.init("hello this is big text corpus");
+    const res = p.str("hello");
+    std.debug.print("target_string = {s}\nresult = {s}\nindex = {d}", .{ res.target_string, res.result.?, res.index });
 }
